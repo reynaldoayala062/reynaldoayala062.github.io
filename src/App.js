b94d59b1f634ff4023af836947609a2476a355ed
reynaldoayala062 <reynaldo-ayala@hotmail.com>
2020-10-28 20:10:43 -0500

    build redux store

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css'; 
 
+import { connect } from 'react-redux';
+
 function App() {
   return (
     <div className="App">
@@ -22,4 +24,4 @@ function App() {
   );
 }
 
-export default App;
+export default connect()(App);
