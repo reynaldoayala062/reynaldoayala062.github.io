001db61aed6ac9f8b50458f60435d5645807ece4
reynaldoayala062 <reynaldo-ayala@hotmail.com>
2020-10-31 11:16:16 -0500

    social media links added to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Linkedin from '../images/Screen Shot 2020-10-31 at 10.41.38 AM.png'
+import Youtube from '../images/Screen Shot 2020-10-31 at 10.42.04 AM.png'
+import Facebook from '../images/Screen Shot 2020-10-31 at 10.42.22 AM.png'
+import Github from '../images/Screen Shot 2020-10-31 at 10.44.57 AM.png'
 
 class NavBar extends React.Component {
 
@@ -11,13 +15,18 @@ class NavBar extends React.Component {
             <div className="navbar">
                 <div className="nav">
                     <div className="nav-item" onClick={this.handleClick} >
-                        <h6> Home </h6>
-                    </div>
-                    <div className="nav-item" onClick={this.handleClick}>
-                        <h6> Project </h6>
-                    </div>
-                    <div className="nav-item" onClick={this.handleClick}>
-                        <h6> Contact </h6>
+                    <a href="https://www.linkedin.com/in/reynaldo-ayala-1b18b172/" target="blank" >
+                        <img src={Linkedin} alt="" href="google.com"/>
+                    </a>
+                    <a href="https://www.youtube.com/channel/UClF70456tB11P5BP5zxNfHw?view_as=subscriber" target="blank" >
+                        <img src={Youtube} alt=""/>
+                    </a>
+                    <a href="https://www.facebook.com/profile.php?id=100004277175364" target="blank" >
+                        <img src={Facebook} alt=""/>
+                    </a>
+                    <a href="https://github.com/reynaldoayala062" target="blank" >
+                        <img src={Github} alt=""/>
+                    </a>
                     </div>
                 </div>
             </div>
--- src/components/project_container.js
+++ src/components/project_container.js
@@ -13,7 +13,7 @@ class ProjectContainer extends React.Component {
                         <iframe title="Finish-Line"width="560" height="315" src="https://www.youtube.com/embed/Qvqbt1LUQOs?start=5" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                     </div>
                     <div className="project-item">
-                    <a href="https://github.com/reynaldoayala062/finish-line-frontend ">Git Hub</a>
+                    <a href="https://github.com/reynaldoayala062/finish-line-frontend" target="blank">Git Hub</a>
                     </div>
                 </div>
                 <br/>
@@ -22,7 +22,7 @@ class ProjectContainer extends React.Component {
                         <iframe title="Mountain-Man"width="560" height="315" src="https://www.youtube.com/embed/NqvEFH_CUa0?start=5" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                     </div>
                     <div className="project-item">
-                    <a href="https://github.com/smithspencer817/mountainman"><h1>Git Hub</h1></a>
+                    <a href="https://github.com/smithspencer817/mountainman" target="blank" ><h1>Git Hub</h1></a>
                     </div>
                 </div>
                 <br/>
@@ -31,8 +31,8 @@ class ProjectContainer extends React.Component {
                         <iframe title="Connecting-The-Dots" width="560" height="315" src="https://www.youtube.com/embed/UMwWLrZC0zs?start=5" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                     </div>
                     <div className="project-item">
-                    <a href="https://github.com/reynaldoayala062/Connecting-the-Dots-Frontend">Git Hub Frontend</a>
-                    <a href="https://github.com/reynaldoayala062/Connecting-the-Dots-Backend">Git Hub Backend</a>
+                    <a href="https://github.com/reynaldoayala062/Connecting-the-Dots-Frontend" target="blank" >Git Hub Frontend</a>
+                    <a href="https://github.com/reynaldoayala062/Connecting-the-Dots-Backend" target="blank" >Git Hub Backend</a>
                     </div>
                 </div>
                 <br/>
@@ -41,7 +41,7 @@ class ProjectContainer extends React.Component {
                         <iframe title="Block-Buster" width="560" height="315" src="https://www.youtube.com/embed/b5PyqvDaQi4?start=5" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> 
                     </div>
                     <div className="project-item">
-                    <a href="https://github.com/reynaldoayala062/block-buster">Git Hub</a>
+                    <a href="https://github.com/reynaldoayala062/block-buster" target="blank" >Git Hub</a>
                     </div>
                 </div>
 
