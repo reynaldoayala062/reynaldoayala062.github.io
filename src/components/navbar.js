ff9967d6bf8d856b58bf2e1308f9689a4e26b259
reynaldoayala062 <reynaldo-ayala@hotmail.com>
2020-10-31 13:09:11 -0500

    added name and medium on navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import Linkedin from '../images/Screen Shot 2020-10-31 at 10.41.38 AM.png'
+import Linkedin from '../images/Screen Shot 2020-10-31 at 12.45.59 PM.png'
 import Youtube from '../images/Screen Shot 2020-10-31 at 10.42.04 AM.png'
-import Facebook from '../images/Screen Shot 2020-10-31 at 10.42.22 AM.png'
+import Facebook from '../images/Screen Shot 2020-10-31 at 12.53.27 PM.png'
 import Github from '../images/Screen Shot 2020-10-31 at 10.44.57 AM.png'
+import Medium from '../images/Screen Shot 2020-10-31 at 12.55.06 PM.png'
 
 class NavBar extends React.Component {
 
@@ -13,7 +14,9 @@ class NavBar extends React.Component {
     render() {
         return(
             <div className="navbar">
+                <h2> Reynaldo Ayala</h2>
                 <div className="nav">
+                    
                     <div className="nav-item">
                     <a href="https://www.linkedin.com/in/reynaldo-ayala-1b18b172/" target="blank" >
                         <img src={Linkedin} alt="" href="google.com"/>
@@ -26,6 +29,9 @@ class NavBar extends React.Component {
                     </a>
                     <a href="https://github.com/reynaldoayala062" target="blank" >
                         <img src={Github} alt=""/>
+                    </a>
+                    <a href="https://medium.com/@reynaldo_ayala" target="blank" >
+                        <img src={Medium} alt=""/>
                     </a>
                     </div>
                 </div>
